a57adc51d889062d93fd9b0669fda680eb9cbe18
DivyaAmbedkar <94296796+DivyaAmbedkar@users.noreply.github.com>
2021-11-23 17:58:24 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,1 +1,55 @@
+/**
+ * @file feature.h
+ * @author Divya
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef FEATURE_H_
+#define FEATURE_H_
+
+
+
+/**
+ * @brief (Declaring all the functions)
+ * 
+ */
+void taxiprint(int);
+void booking(struct booking *b,struct taxi t,int no)
+void decrement()
+void taxiprint(int taxino)
+void display()
+  
+  
+  /**
+ * @brief (Creating a structure for booking taxi)
+ * 
+ */
+struct taxi
+    {
+        int customer_id;
+        char pickup_point[];
+        char drop_point[];
+        int pickup_time;
+  };
+
+
+ /**
+ * @brief (Structure to get booking details from the user)
+ * 
+ */
+struct booking
+    {
+    
+        int booking_id;
+        int customer_id;
+        char from[];
+        char to[];
+        int pickuptime;
+        int droptime;
+        int amount;
+    };
 
