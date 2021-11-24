1c623ffa4de25a75e840db42e5f5763f223d3559
DivyaAmbedkar <94296796+DivyaAmbedkar@users.noreply.github.com>
2021-11-24 22:45:02 +0530

    Update and rename README.md to test_taxi.c

---
+++ 3_Implementation/test/test_taxi.c
@@ -0,0 +1,39 @@
+#include"taxi.h"
+#include"unity_internals.h"
+#include"unity.h"
+
+void setUp(){}
+
+void tearDown(){}
+
+void test_book(void){
+    TEST_ASSERT_EQUAL();
+}
+
+void test_display(void){
+    TEST_ASSERT_EQUAL();
+}
+
+void test_init(void){
+    TEST_ASSERT_EQUAL();
+    
+}
+
+
+
+void test_max_earning(void){
+    TEST_ASSERT_EQUAL();
+}        
+
+
+int main(){
+    UNITY_BEGIN();
+
+    RUN_TEST(test_book);
+    RUN_TEST(test_display);
+    RUN_TEST(test_init);
+    RUN_TEST(test_max_earning);
+    
+    
+    return UNITY_END();
+}
